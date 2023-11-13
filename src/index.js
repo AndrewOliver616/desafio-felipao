c10e7299cbc87e5b9b06610dc36ca5ec95ad199f
AndrewOliver616 <andre.oliveira.backup@outlook.com>
2023-11-13 00:34:29 -0300

    enviando projeto para o github

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
-console.log("Olá, mundo")
+let heroi = "Super Teacher Felipão"
+let experienciaXP = 9000
+let nivelHeroi = ""
+
+if (experienciaXP <1000) {
+    nivelHeroi = " Nivel Ferro"
+} else if (experienciaXP<= 2000) {
+    nivelHeroi = " Nivel Bronze"
+}else if (experienciaXP<= 5000) {
+    nivelHeroi = " Nivel Prata"
+}else if (experienciaXP<= 7000) {
+    nivelHeroi = " Nivel Ouro"
+}else if (experienciaXP<= 8000) {
+    nivelHeroi = " Nivel Platina"
+}else if (experienciaXP<= 9000) {
+    nivelHeroi = " Nivel Ascendente"
+}else if (experienciaXP <= 10000) {
+    nivelHeroi = " Nivel Imortal"
+}else if (experienciaXP > 10000) {
+    nivelHeroi = " Nivel Radiamte"
+}
+
+console.log("O Herói " + heroi + " está com XP de " + experienciaXP + nivelHeroi)
